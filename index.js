da58d4c4ff9ebc893b22726eca8443806e5aeae8
Elijah <bundielly@gmail.com>
2022-07-01 11:33:56 +0300

    Check-Out-cart table 90% complete

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
 // Code goes here
 
-// Adding EventListeners to the pop up window
+// ADDING EVENTLISTENERS TO POPUP WINDOWS
+
 let checkOutBtn = document.querySelector('.final .check-out')
 let proceedBtn = document.querySelector('.proceed')
 let backBtn = document.querySelector('.go-back')
 let overlay = document.querySelector('.overlay')
-
 // Define the popup window
 const checkOutWindow = document.querySelector('.popup-window')
-
 // open the popup window
 checkOutBtn.addEventListener('click', (e) => {    
     openPopupWindow(checkOutWindow)
 })
-
 // close the popup window
 backBtn.addEventListener('click', (e) => {
     closePopupWindow(checkOutWindow)
 })
-
 // add eventListener to the outward overlay to close the popup window
 overlay.addEventListener('click', (e) => {
     closePopupWindow(checkOutWindow)
 })
-
 // Opening function
 function openPopupWindow(window) {
     if (window === null) {
@@ -33,7 +29,6 @@ function openPopupWindow(window) {
         overlay.classList.add('active') 
     }   
 }
-
 // Closing function
 function closePopupWindow(window) {
     if (window === null) {
@@ -46,7 +41,8 @@ function closePopupWindow(window) {
 
 
 
-// Drop-down menu functions
+// DROP-DOWN MENU FUNCTIONS
+
 // Definitions
 let list = document.querySelector('.list')
 let dropDown = document.querySelector('.drop-down')
@@ -56,14 +52,46 @@ let trHeading = document.querySelector('.heading')
 let finalList = document.querySelector('.final-list')
 
 // Creating a table for the gotten elements
-function createTable(matName, qty) {
+
+let trHeader = document.createElement('tr')    
+let th1 = document.createElement('th')
+let th2 = document.createElement('th')
+let th3 = document.createElement('th')
+let th4 = document.createElement('th')
+th1.innerText = 'MATERIAL'
+th2.innerText = 'QTY'
+th3.innerText = 'COST'
+th4.innerText = 'TOTAL'
+
+trHeader.appendChild(th1)
+trHeader.appendChild(th2)
+trHeader.appendChild(th3)
+trHeader.appendChild(th4)
+
+finalList.appendChild(trHeader)
+console.log(finalList)
+
+// Table appending function
+function createTable(matName, qty, cost, total) {
+
     let tr = document.createElement('tr')
     let td1 = document.createElement('td')
     let td2 = document.createElement('td')
+    let td3 = document.createElement('td')
+    let td4 = document.createElement('td')
+    td4.className = 'bold'
     td1.innerText = matName
     td2.innerText = `${qty} pcs`
-    finalList.appendChild(tr)
+    td3.innerText = `Ksh ${cost}`
+    td4.innerText = `Ksh ${total}`
+
     tr.appendChild(td1)
+    tr.appendChild(td2)
+    tr.appendChild(td3)
+    tr.appendChild(td4)
+
+    finalList.appendChild(tr)
+    
 }
 
 
@@ -80,7 +108,10 @@ fetch('http://localhost:3000/materials')
         li.addEventListener('click', (e) => {
             image.src = material.image
             let li2 = material.name
-            createTable(li2, 100)
+            let qty = 100
+            let cost = material.discount_price
+            let total = qty * cost
+            createTable(li2, qty, cost, total)
             // li2.textContent = material.name
             // finalList.appendChild(li2)
         })     
@@ -118,13 +149,20 @@ let actPrice = document.querySelector('.act-price').value
 let imgUrl = document.querySelector('.img-url').src
 let addSubmit = document.querySelector('.add')
 
-function postData() {
+
+        
+
+
+addSubmit.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
     fetch('http://localhost:3000/materials', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body:JSON.stringify({
+
             name: material,
             description_1: descr1,
             description_2: descr2,
@@ -137,12 +175,7 @@ function postData() {
     .then(res => res.json())
     .then(data => {
         console.log(data)
-    })    
-}
-
-addSubmit.addEventListener('submit', (e) => {
-    e.preventDefault()
-    postData()
+    })
 })
 
 
